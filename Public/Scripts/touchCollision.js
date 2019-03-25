e03d00cc98143cb943e062ce91b665d1eb560741
Beston <beston@modiface.com>
2019-03-25 12:38:26 -0400

    updated

--- Public/Scripts/touchCollision.js
+++ Public/Scripts/touchCollision.js
@@ -1,46 +1,28 @@
 // @input Asset.Material material
 // @input Component.Label label
-
-// Arrays are indexed from 0.
-// Disable everything but the first (0th) item .
-// for (var i = 1; i < script.items.length; i++) {
-//   script.items[i].enabled = false;
-// }
-
-// // We remember what item is currently visible.
-// // When we start it's the 0th item.
-// var currentItemIndex = 0;
-
-// // Define what happens when you tap.
-// function activateNextItem() {
-//   // Disable the current item.
-//   script.items[currentItemIndex].enabled = false;
-
-//   // Increment the current item index
-//   currentItemIndex += 1;
-
-//   // We need the current item index to wrap around
-//   // once it's higher than the number of items we have.
-//   currentItemIndex = currentItemIndex % script.items.length;
-
-//   // Enable the new item.
-//   script.items[currentItemIndex].enabled = true;
-// }
-
-// // Bind the function to the touch event.
-// var touchEvent = script.createEvent("TapEvent");
-// touchEvent.bind(activateNextItem);
-
+// @input Component.Label blendModeLabel
+var blendModes = [
+  "Normal",
+  "Screen",
+  "PreMultAlpha",
+  "AlphaToCoverage",
+  "Add",
+  "AlphaTest",
+  "ColouredGlass",
+  "Multiply"
+];
 var pass = script.material.getPass(0);
-var i = 0;
 function changeColor() {
-  print("here");
-  print(script.material.getPassCount());
   pass.baseColor = new vec4(Math.random(), Math.random(), Math.random(), 1.0);
-  pass.blendMode = i;
-  i++;
-  if (i >= 7) i = 0;
-  script.label.text = "render" + i;
 }
 var touchEvent1 = script.createEvent("TapEvent");
 touchEvent1.bind(changeColor);
+
+global.changeRendering = function(index) {
+  pass.blendMode = index;
+  script.label.text = "Render " + (index + 1) + "/8";
+  script.blendModeLabel.text = "Render Mode: " + blendModes[index];
+  print(index);
+};
+script.label.text = "Render 1/8";
+ script.blendModeLabel.text = "Render Mode: " + blendModes[0];
